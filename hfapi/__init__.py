2a1ccda7ea66d6884c2550b56f922ea2392274a5
Alex Rush <sasha.rush@gmail.com>
2020-07-14 17:30:08 -0400

    Hugging face api client

---
+++ hfapi/__init__.py
@@ -0,0 +1,27 @@
+import requests
+
+class Client:
+    def __init__(self):
+        pass
+
+    def _call(self, query, model):
+        model_location = "https://api-inference.huggingface.co/models/" + model
+        return requests.post(model_location, json=query).json()
+
+    def qa(self, question, context,
+           model="distilbert-base-uncased-distilled-squad"):
+        query = dict(question=question, context=context)
+        return self._call(query, model)
+
+    def summarize(self, input, model="sshleifer/distilbart-xsum-12-6"):
+        return self._call(input, model)
+
+    def text_generate(self, input, model="distilgpt2"):
+        return self._call(input, model)
+
+    def mask_fill(self, input, model="distilbert-base-uncased"):
+        return self._call(input, model)
+
+    def classify(input,
+                 model="distilbert-base-uncased-finetuned-sst-2-english"):
+        return self._call(input, model)
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+setup(name='HF API',
+      version='0.1',
+      description='Hugging Face Python API',
+      packages=['hfapi']
+)
