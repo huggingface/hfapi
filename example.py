f2c1405150f3f83396519ee42b40599ad06a7aaf
Alex Rush <sasha.rush@gmail.com>
2020-07-14 17:32:06 -0400

    Hugging face api client

--- example.py
+++ example.py
@@ -1,10 +1,18 @@
 import hfapi
-client = hfapi.HFClient()
+client = hfapi.Client()
 
+print("""client.qa("Where does she live?", "She lives in Berlin."))""")
 print(client.qa("Where does she live?", "She lives in Berlin."))
+print()
 
+print("""client.text_generate("My name is Julien and I like to ")""")
 print(client.text_generate("My name is Julien and I like to "))
+print()
 
+print("""client.summarize("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.")""")
 print(client.summarize("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."))
+print()
 
+print("""client.mask_fill("Paris is the [MASK] of France."))""")
 print(client.mask_fill("Paris is the [MASK] of France."))
+print()
