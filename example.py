2f07c1e24e576609118e91f20123686b27be5ff0
Alex Rush <sasha.rush@gmail.com>
2020-07-15 14:38:27 -0400

    update model names and readme

--- example.py
+++ example.py
@@ -1,23 +1,71 @@
 import hfapi
 client = hfapi.Client()
 
-print("""client.qa("Where does she live?", "She lives in Berlin."))""")
-print(client.qa("Where does she live?", "She lives in Berlin."))
+
+import hfapi
+client = hfapi.Client()
+print("""
+
+```python
+import hfapi
+client = hfapi.Client()
+```
+
+""")
+
+print("""```python
+client.question_answering("Where does she live?", "She lives in Berlin.")
+```
+""")
+print("```")
+print(client.question_answering("Where does she live?", "She lives in Berlin."))
+print("```")
 print()
 
-print("""client.text_generate("My name is Julien and I like to ")""")
-print(client.text_generate("My name is Julien and I like to "))
+print("""```python
+client.text_generation("My name is Julien and I like to ")
+```
+""")
+print("```")
+print(client.text_generation("My name is Julien and I like to "))
+print("```")
 print()
 
-print("""client.summarize("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.")""")
-print(client.summarize("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."))
+print("""```
+client.summarization("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.")
+```
+""")
+print("```")
+print(client.summarization("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."))
+print("```")
 print()
 
-print("""client.mask_fill("Paris is the [MASK] of France."))""")
-print(client.mask_fill("Paris is the [MASK] of France."))
+print("""```python
+client.fill_mask("Paris is the [MASK] of France."))
+```
+""")
+print("```")
+print(client.fill_mask("Paris is the [MASK] of France."))
+print("```")
 print()
 
 
-print("""client.classify("I hated the movie!")""")
-print(client.classify("I hated the movie!"))
+print("""```python
+client.text_classification("I hated the movie!")
+```
+""")
+
+print("```")
+print(client.text_classification("I hated the movie!"))
+print("```")
+print()
+
+
+print("""```python
+client.token_classification("My name is Sarah and I live in London")
+```
+""")
+print("```")
+print(client.token_classification("My name is Sarah and I live in London"))
+print("```")
 print()
--- hfapi/__init__.py
+++ hfapi/__init__.py
@@ -8,20 +8,23 @@ class Client:
         model_location = "https://api-inference.huggingface.co/models/" + model
         return requests.post(model_location, json=query).json()
 
-    def qa(self, question, context,
+    def question_answering(self, question, context,
            model="distilbert-base-uncased-distilled-squad"):
         query = dict(question=question, context=context)
         return self._call(query, model)
 
-    def summarize(self, input, model="sshleifer/distilbart-xsum-12-6"):
+    def summarization(self, input, model="sshleifer/distilbart-xsum-12-6"):
         return self._call(input, model)
 
-    def text_generate(self, input, model="distilgpt2"):
+    def text_generation(self, input, model="distilgpt2"):
         return self._call(input, model)
 
-    def mask_fill(self, input, model="distilbert-base-uncased"):
+    def fill_mask(self, input, model="distilbert-base-uncased"):
         return self._call(input, model)
 
-    def classify(self, input,
+    def text_classification(self, input,
                  model="distilbert-base-uncased-finetuned-sst-2-english"):
+        return self._call(input, model)
+
+    def token_classification(self, input, model="dslim/bert-base-NER"):
         return self._call(input, model)
