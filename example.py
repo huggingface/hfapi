721e748dedc43ad72405f1004bbf6074c6d29290
Alex Rush <sasha.rush@gmail.com>
2020-07-15 14:43:27 -0400

    update model names and readme

--- example.py
+++ example.py
@@ -17,17 +17,15 @@ print("""```python
 client.question_answering("Where does she live?", "She lives in Berlin.")
 ```
 """)
-print("```")
-print(client.question_answering("Where does she live?", "She lives in Berlin."))
-print("```")
-print()
+
+print(">", client.question_answering("Where does she live?", "She lives in Berlin."))
 
 print("""```python
 client.text_generation("My name is Julien and I like to ")
 ```
 """)
 print("```")
-print(client.text_generation("My name is Julien and I like to "))
+print(">", client.text_generation("My name is Julien and I like to "))
 print("```")
 print()
 
@@ -35,18 +33,16 @@ print("""```
 client.summarization("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct.")
 ```
 """)
-print("```")
-print(client.summarization("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."))
-print("```")
+
+print(">", client.summarization("The tower is 324 metres (1,063 ft) tall, about the same height as an 81-storey building, and the tallest structure in Paris. Its base is square, measuring 125 metres (410 ft) on each side. During its construction, the Eiffel Tower surpassed the Washington Monument to become the tallest man-made structure in the world, a title it held for 41 years until the Chrysler Building in New York City was finished in 1930. It was the first structure to reach a height of 300 metres. Due to the addition of a broadcasting aerial at the top of the tower in 1957, it is now taller than the Chrysler Building by 5.2 metres (17 ft). Excluding transmitters, the Eiffel Tower is the second tallest free-standing structure in France after the Millau Viaduct."))
 print()
 
 print("""```python
 client.fill_mask("Paris is the [MASK] of France."))
 ```
 """)
-print("```")
-print(client.fill_mask("Paris is the [MASK] of France."))
-print("```")
+
+print(">",client.fill_mask("Paris is the [MASK] of France."))
 print()
 
 
@@ -55,9 +51,7 @@ client.text_classification("I hated the movie!")
 ```
 """)
 
-print("```")
-print(client.text_classification("I hated the movie!"))
-print("```")
+print(">", client.text_classification("I hated the movie!"))
 print()
 
 
@@ -65,7 +59,6 @@ print("""```python
 client.token_classification("My name is Sarah and I live in London")
 ```
 """)
-print("```")
-print(client.token_classification("My name is Sarah and I live in London"))
-print("```")
+
+print(">", client.token_classification("My name is Sarah and I live in London"))
 print()
